98e0ef28a38a0236e76bf2e42ab8579d466e6635
LeoNeeDpk1 <leoneedpk1@gmail.com>
2018-01-31 17:23:34 +1200

    - Наброски для будущей переделки

---
+++ pidor.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf8
+import os
+from random import randint
+import time
+from ConfigParser import ConfigParser
+
+def ParamsRead(section, key): #Парсер config.ini
+    parameter = ConfigParser()
+    parameter.read('/home/fku-server/pidor_roulette/results.ini')
+    return parameter.get(section, key)
+
+def stats(p): #Запись статистики
+    statscheck = ConfigParser()
+    statscheck.read('/home/fku-server/pidor_roulette/results.ini')
+    if not p == "GENERAL":
+        if statscheck.has_section(p) == False:
+            statscheck.add_section(p)
+            with open('/home/fku-server/pidor_roulette/results.ini', 'w') as statfile:
+                statscheck.write(statfile)
+        if statscheck.has_option(p, 'num') == False:
+            statscheck.set(p,'num',"1")
+            with open('/home/fku-server/pidor_roulette/results.ini', 'w') as statfile:
+                statscheck.write(statfile)
+        else:
+            statscheck.set(p,'num', (int(ParamsRead(p, 'num'))+ 1))
+            with open('/home/fku-server/pidor_roulette/results.ini', 'w') as statfile:
+                statscheck.write(statfile)
+
+def topchart():
+    lastsection = ""
+    t = ""
+    l = []
+    plist = ConfigParser()
+    plist.read('/home/fku-server/pidor_roulette/results.ini')
+    for section_name in plist.sections():
+        for(num, count) in plist.items(section_name):
+            q = [int(count), str(section_name)]
+            l.append(q)
+    e = sorted(l, key=lambda x: str(x[0]), reverse=True)
+    for item in e:
+        t = t + str(str(item[1]) + " = " + str(item[0])) + "\n"
+    return t
+
+with open('/home/fku-server/pidor_roulette/pidors') as p:
+    plines = p.read().split(',')
+with open('/home/fku-server/pidor_roulette/quotes') as q:
+    qlines = q.read().split('*')
+with open('/home/fku-server/pidor_roulette/quotes2') as q2:
+    q2lines = q2.read().split('*')
+
+quote = qlines[randint(0, len(qlines)-1)]
+quote2 = q2lines[randint(0, len(q2lines)-1)]
+pidor = plines[randint(0, len(plines)-1)]
+
+pidor = str(pidor).replace("\n", "")
+stats(str(pidor))
+
+os.system('curl -s -X POST https://api.telegram.org/bot529103954:AAHX31LDNS_BYhruFUyxLLjbFa_rPQxhhMI/sendMessage -d chat_id=-1001088067576 -d text="' + str(quote) + '"')
+time.sleep(randint(1, 3))
+os.system('curl -s -X POST https://api.telegram.org/bot529103954:AAHX31LDNS_BYhruFUyxLLjbFa_rPQxhhMI/sendMessage -d chat_id=-1001088067576 -d text="' + str(quote2) + '"')
+time.sleep(randint(1, 3))
+os.system('curl -s -X POST https://api.telegram.org/bot529103954:AAHX31LDNS_BYhruFUyxLLjbFa_rPQxhhMI/sendMessage -d chat_id=-1001088067576 -d parse_mode=html -d text="Пидор обнаружен! И это: <b>' + str(pidor) + '</b>"')
+os.system('curl -s -X POST https://api.telegram.org/bot529103954:AAHX31LDNS_BYhruFUyxLLjbFa_rPQxhhMI/sendMessage -d chat_id=-1001088067576 -d parse_mode=html -d text="<b> Доска позора:</b>\n' + str(topchart()) + '"')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
