8150d8d433eda6465c8e82745a8583dec63945a5
LeoNeeDpk1 <leoneedpk1@gmail.com>
2018-02-01 16:07:02 +1200

    Рабочая версия

--- pidor.py
+++ pidor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf8
-import config
+import config as c
 import os
 import time
 from random import randint
@@ -12,90 +12,62 @@ curdir = os.path.dirname(os.path.abspath(__file__))
 
 def resultsread(section, key):  # Парсер results.ini
     result = ConfigParser()
-    result.read('/home/fku-server/pidor_roulette/results.ini')
+    result.read(curdir + '/results.ini')
     return result.get(section, key)
 
 
-def stats(p): #Запись статистики
+def stats(p):  # Запись статистики
     statscheck = ConfigParser()
-    statscheck.read('/home/fku-server/pidor_roulette/results.ini')
+    statscheck.read(curdir + '/results.ini')
     if not p == "GENERAL":
-        if statscheck.has_section(p) == False:
+        if statscheck.has_section(p) is False:
             statscheck.add_section(p)
-            with open('/home/fku-server/pidor_roulette/results.ini', 'w') as statfile:
+            with open(curdir + '/results.ini', 'w') as statfile:
                 statscheck.write(statfile)
-        if statscheck.has_option(p, 'num') == False:
-            statscheck.set(p,'num',"1")
-            with open('/home/fku-server/pidor_roulette/results.ini', 'w') as statfile:
+        if statscheck.has_option(p, 'num') is False:
+            statscheck.set(p, 'num', "1")
+            with open(curdir + '/results.ini', 'w') as statfile:
                 statscheck.write(statfile)
         else:
-            statscheck.set(p,'num', (int(resultsread(p, 'num'))+ 1))
-            with open('/home/fku-server/pidor_roulette/results.ini', 'w') as statfile:
+            statscheck.set(p, 'num', str((int(resultsread(p, 'num')) + 1)))
+            with open(curdir + '/results.ini', 'w') as statfile:
                 statscheck.write(statfile)
 
+
 def topchart():
-    lastsection = ""
-    t = ""
+    chart = '<b>=== ТОП пидоров ===</b>\n'
     l = []
     plist = ConfigParser()
-    plist.read('/home/fku-server/pidor_roulette/results.ini')
+    plist.read(curdir + '/results.ini')
     for section_name in plist.sections():
         for(num, count) in plist.items(section_name):
             q = [int(count), str(section_name)]
             l.append(q)
     e = sorted(l, key=lambda x: str(x[0]), reverse=True)
+    best = e[0][0]
     for item in e:
-        t = t + str(str(item[1]) + " = " + str(item[0])) + "\n"
-    return t
+        if item[0] == best:
+            chart = chart + str(str(item[1]) + " = " + str(item[0])) + " " + u'\U0001F451' + "\n"
+        else:
+            chart = chart + str(str(item[1]) + " = " + str(item[0])) + "\n"
+    return chart
 
-with open('/home/fku-server/pidor_roulette/pidors') as p:
-    plines = p.read().split(',')
-with open('/home/fku-server/pidor_roulette/quotes') as q:
-    qlines = q.read().split('*')
-with open('/home/fku-server/pidor_roulette/quotes2') as q2:
-    q2lines = q2.read().split('*')
 
-quote = qlines[randint(0, len(qlines)-1)]
-quote2 = q2lines[randint(0, len(q2lines)-1)]
-pidor = plines[randint(0, len(plines)-1)]
+def sendtotg(text):
+    os.system('curl -s -X POST https://api.telegram.org/bot' + c.token + '/sendMessage -d chat_id=' + c.chatid +
+              ' -d parse_mode=html -d text="' + str(text) + '"')
 
-pidor = str(pidor).replace("\n", "")
-stats(str(pidor))
 
-'''os.system('curl -s -X POST https://api.telegram.org/bot/sendMessage -d chat_id= -d text="' + str(quote) + '"')
+pidor = c.pidors[randint(0, len(c.pidors)-1)]
+startphrase = c.startphrase[randint(0, len(c.startphrase)-1)]
+searchphrase = c.searchphrase[randint(0, len(c.searchphrase)-1)]
+foundphrase = str(c.foundphrase[randint(0, len(c.foundphrase)-1)] % pidor)
+
+stats(pidor)
+
+sendtotg(startphrase)
 time.sleep(randint(1, 3))
-os.system('curl -s -X POST https://api.telegram.org/bot/sendMessage -d chat_id= -d text="' + str(quote2) + '"')
+sendtotg(searchphrase)
 time.sleep(randint(1, 3))
-os.system('curl -s -X POST https://api.telegram.org/bot/sendMessage -d chat_id= -d parse_mode=html -d text="Пидор обнаружен! И это: <b>' + str(pidor) + '</b>"')
-os.system('curl -s -X POST https://api.telegram.org/bot/sendMessage -d chat_id= -d parse_mode=html -d text="<b> Доска позора:</b>\n' + str(topchart()) + '"')
-'''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+sendtotg(foundphrase)
+sendtotg(topchart())
