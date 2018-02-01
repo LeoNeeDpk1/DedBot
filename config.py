18a8422d2bebddae0a3ce7f0000a515899ed9b99
LeoNeeDpk1 <35986338+LeoNeeDpk1@users.noreply.github.com>
2018-02-01 17:56:35 +1200

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ chatid = '<Chat_ID>'
 
 pidors = ['name1', 'name2', '...']
 
-startphrase = ['phrase1', 'phrase2', '..']
+startphrase = ['phrase1', 'phrase2', '...']
 
-searchphrase = ['phrase1', 'phrase2', '..']
+searchphrase = ['phrase1', 'phrase2', '...']
 
-foundphrase = ['phrase1, <b>%s</b>', '<b>%s</b>, phrase2', '..'] # Вместо %s будет подставляться имя того, кого выбрал рандом. parse_mode=html.
+foundphrase = ['phrase1, <b>%s</b>', '<b>%s</b>, phrase2', '...'] # Вместо %s будет подставляться имя того, кого выбрал рандом. parse_mode=html.
